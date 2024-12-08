ca824786969e8033ebd9382de81d81e58725ba7a
JakeBrowning90 <jacobbrowning90@gmail.com>
2024-12-08 14:36:04 -0500

    Add file Create query

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -8,6 +8,17 @@ const prisma = new PrismaClient();
 exports.createFile = asyncHandler(async (req, res) => {
   console.log("File created");
   console.log(req.file);
+  console.log(req.user);
 
+  await prisma.file.create({
+    data: {
+      name: req.file.filename,
+      size: req.file.size,
+      url: req.file.path,
+      owner: {
+        connect: { id: req.user.id },
+      },
+    },
+  });
   res.redirect("/");
 });
